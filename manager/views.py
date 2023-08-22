3d3610df2c296ca4cff26349ba56ab05dbe681d5
cathsuchurch <cathsuchurch@gmail.com>
2023-08-22 10:24:57 +0000

    change

--- manager/views.py
+++ manager/views.py
@@ -43,21 +43,30 @@ def programs(request):
     for prog in Program.objects.all().order_by('start_date'):
         prog_box.append(fix_prog_json(prog))
     
+
+    # print(Program.objects.all())
+    for prog in Program.objects.all():
+        new_prog = Program()
+        new_prog.name = prog.name
+        new_prog.description = prog.description
+        new_prog.image = prog.image
+        new_prog.host = prog.host
+        new_prog.start_date = prog.start_date + timedelta(days=1)
+        new_prog.end_date = prog.end_date + timedelta(days=1)
+        new_prog.save()
+
     if request.method == 'POST':
-        if Program.objects.filter(name=request.POST.get('name').upper()).exists():
-           messages.error(request,'program already exists')
+        if int(request.POST['id']) == 0:
+            program = create_program(request, Program())
+            program.image = request.FILES['image']
+            program.save()
+            return redirect('programs_page')
         else:
-            if int(request.POST['id']) == 0:
-                program = create_program(request, Program())
+            program = create_program(request, Program.objects.get(id=int(request.POST.get('id'))))
+            if 'image' in request.FILES:
                 program.image = request.FILES['image']
-                program.save()
-                return redirect('programs_page')
-            else:
-                program = create_program(request, Program.objects.get(id=int(request.POST.get('id'))))
-                if 'image' in request.FILES:
-                    program.image = request.FILES['image']
-                program.save()
-                return redirect('programs_page')
+            program.save()
+            return redirect('programs_page')
     context = {
         'token' : get_token(request),
         'prog_box' : json.dumps(prog_box)
